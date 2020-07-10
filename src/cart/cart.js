0587ae2bc6db880bf38b536f31ee2c23ab19717b
Ismayil <hajiev93@gmail.com>
2020-07-10 10:10:06 +0300

    rep ready

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -1,62 +1,66 @@
-import * as basicLightbox from 'basiclightbox';
-import dishes from '../menu.json';
-const cartRef = document.querySelector('.cart');
-
-// const instance = basicLightbox.create(document.querySelector('template'));
+// import * as basicLightbox from 'basiclightbox';
+// import dishes from '../menu.json';
 // const cartRef = document.querySelector('.cart');
+
+// // const instance = basicLightbox.create(document.querySelector('template'));
+// // const cartRef = document.querySelector('.cart');
+// // cartRef.addEventListener('click', instance.show);
+// const name = 'Ismayil';
+
+// const instance = basicLightbox.create(
+//   `
+//     <div class="modal">
+//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
+//         <p>${name}</p>
+//         <a class="closeBtn">Close</a>
+//     </div>
+// `,
+//   {
+//     onShow: instance => {
+//       instance.element().querySelector('a').onclick = instance.close;
+//     },
+//   },
+// );
+
 // cartRef.addEventListener('click', instance.show);
-const name = 'Ismayil';
 
-const instance = basicLightbox.create(
-  `
-    <div class="modal">
-        <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-        <p>${name}</p>
-        <a class="closeBtn">Close</a>
-    </div>
-`,
-  {
-    onShow: instance => {
-      instance.element().querySelector('a').onclick = instance.close;
-    },
-  },
-);
+// //======addProduct
 
-cartRef.addEventListener('click', instance.show);
+// const addToCartRef = document.querySelector('.card__button');
 
-//======addProduct
+// const menuRef = document.querySelector('.menu');
 
-const addToCartRef = document.querySelector('.card__button');
+// menuRef.addEventListener('click', addToCartFn);
 
-const menuRef = document.querySelector('.menu');
+// const cart = {
+//   products: [],
+// };
+// let newProduct = {
+//   name: '',
+//   price: 0,
+//   id: '',
+//   quantity: 0,
+// };
 
-menuRef.addEventListener('click', addToCartFn);
+// function addToCartFn(e) {
+//   const dishId = e.target.previousElementSibling.dataset.id;
 
-const cart = {
-  products: [],
-};
+//   const chosenDish = dishes.find(dish => dish.id === dishId);
 
-function addToCartFn(e) {
-  console.log(e.target);
-  const dishId = e.target.previousElementSibling.dataset.id;
+//   for (let prod of cart.products) {
+//     if (prod.id !== dishId) {
+//       newProduct = {
+//         name: chosenDish.name,
+//         price: chosenDish.price,
+//         id: chosenDish.id,
+//         quantity: 1,
+//       };
+//     } else {
+//       const newCartProducts = cart.products.map(prod => (prod.quantity += 1));
+//       console.log(newCartProducts);
+//     }
+//   }
+//   cart.products = [newProduct, ...cart.products];
 
-  const chosenDish = dishes.find(dish => dish.id === dishId);
-
-  for (let prod of cart.products) {
-    if (prod.id === dishId) {
-      prod.quantity += 1;
-      let newArr = [...cart.products];
-      cart.products = newArr;
-      console.log(cart.products);
-    }
-  }
-  const newProduct = {
-    name: chosenDish.name,
-    price: chosenDish.price,
-    id: chosenDish.id,
-    quantity: 1,
-  };
-  cart.products = [newProduct, ...cart.products];
-
-  //   console.log(cart.products);
-}
+//   // console.log(cart.products);
+// }
